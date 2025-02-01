20879f5756a30aa847f0ed9aeb8a154e56a0545a
Arkhalisal <wf51182474@gmail.com>
2025-02-01 21:39:07 +0800

    file fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 // this is the home page
 
 // local imports
-import Layout from "../components/Layout";
+import Layout from "@/components/Layout";
 import PlanetsList from "@/components/PlanetsList";
 
 export default function Home() {
